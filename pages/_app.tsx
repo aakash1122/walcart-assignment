cea1ed70fd2288146018cc7a5eba681ae64a803d
aakash1122 <aakaashhossain@gmail.com>
2022-05-23 22:05:11 +0600

    ⚒️ configured graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { store } from "../store";
 import { Provider } from "react-redux";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />;
+      </ApolloProvider>
     </Provider>
   );
 }
